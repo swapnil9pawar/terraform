baab86f6fcf5c62424aa7411649e34887b7b18e7
swapnil pawar <104321535+swapnil9pawar@users.noreply.github.com>
2024-12-29 16:01:59 +0530

    Update variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -30,3 +30,15 @@ variable "public_subnet_az2_cidr" {
   type    = string
   default = "10.0.2.0/24"
 }
+ # CIDR of private subnet in AZ1
+ variable "private_subnet_az1_cidrr" {
+ type = string
+ default = "10.0.3.0/24"
+}
+
+ # CIDR of private subnet in AZ2
+ variable "private_subnet_az2_cidrr" {
+ type = string
+ default = "10.0.4.0/24"
+}
+
