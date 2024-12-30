cac4ac6c56869e23e32eaf9dd5acdd3591252572
swapnil pawar <104321535+swapnil9pawar@users.noreply.github.com>
2024-12-30 23:39:16 +0530

    Delete key-pair directory

--- key-pair/main.tf
+++
@@ -1,5 +0,0 @@
-# Fetching AWS Key Pair
-data "aws_key_pair" "splunk_key" {
-  key_name           = var.key_name
-  include_public_key = true
-}
--- key-pair/output.tf
+++
@@ -1,4 +0,0 @@
-# Key Pair ID
-output "id" {
-  value = data.aws_key_pair.splunk_key.id
-}
