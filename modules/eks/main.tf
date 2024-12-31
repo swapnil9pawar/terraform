50777595c6a496aae3bbff06d1f0b0258cef6efb
swapnil pawar <104321535+swapnil9pawar@users.noreply.github.com>
2024-12-31 16:03:30 +0530

    Update main.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -12,6 +12,18 @@ resource "aws_eks_cluster" "eks" {
     value = var.type
   }
 }
+resource "aws_eks_addon" "addons" {
+  for_each                = { for addon in var.addons : addon.name => addon }
+  cluster_name            = var.cluster_name
+  addon_name              = each.value.name
+  addon_version           = each.value.version
+  resolve_conflicts_on_update = "OVERWRITE"
+
+depends_on = [
+    aws_eks_cluster.eks
+  ]
+}
+
 
 # Using Data Source to get all Avalablility Zones in Region
 data "aws_availability_zones" "available_zones" {}
