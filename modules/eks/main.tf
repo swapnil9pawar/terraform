88258df08d50b67e683f367ac9a17f16a7d08f51
swapnil pawar <104321535+swapnil9pawar@users.noreply.github.com>
2025-01-05 21:36:42 +0530

    Update main.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -12,17 +12,28 @@ resource "aws_eks_cluster" "eks" {
     value = var.type
   }
 }
-resource "aws_eks_addon" "addons" {
-  for_each                = { for addon in var.addons : addon.name => addon }
-  cluster_name            = var.cluster_name
-  addon_name              = each.value.name
-  addon_version           = each.value.version
-  resolve_conflicts_on_update = "OVERWRITE"
+#resource "aws_eks_addon" "aws-ebs-csi-driver" {
+#  cluster_name  = var.cluster_name
+#  addon_name    = "aws-ebs-csi-driver"
+#  addon_version = var.aws-ebs-csi-driver_version
+#}
 
-depends_on = [
-    aws_eks_cluster.eks
-  ]
+resource "aws_eks_addon" "coredns" {
+  cluster_name  = var.cluster_name
+  addon_name    = "coredns"
+  addon_version = var.coredns_version
+  depends_on    = [aws_eks_cluster.eks]
+
 }
+
+resource "aws_eks_addon" "kube_proxy" {
+  cluster_name  = var.cluster_name
+  addon_name    = "kube-proxy"
+  addon_version = var.kube_proxy_version
+  depends_on    = [aws_eks_cluster.eks]
+
+}
+
 
 
 # Using Data Source to get all Avalablility Zones in Region
